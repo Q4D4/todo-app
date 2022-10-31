5068012b25d37af8c36ad871c6f28abe40a464dd
DavidKadaria <david.qada@gmail.com>
2022-10-31 23:14:03 +0400

    Design completed; Added theme-color dynamic changing logic

--- js/theme.js
+++ js/theme.js
@@ -2,11 +2,14 @@ const main = function () {
 	// References
 	const themeSwitch = document.getElementById('theme-switch');
 	const html = document.documentElement;
+	const themeColor = document.querySelector("[name='theme-color'");
 	// Current theme
 	let currentTheme = getTheme();
 	// Constants
 	const defaultTheme = 'light';
 	const transitionSetDelay = 300;
+	const bgLight = 'hsl(236, 33%, 92%)';
+	const bgDark = 'hsl(235, 21%, 11%)';
 
 	// Check if theme value is set
 	if (!currentTheme) {
@@ -16,6 +19,8 @@ const main = function () {
 
 	// Add class for the first time
 	html.classList.add(currentTheme);
+	// Set meta 'theme-color'
+	themeColor.content = currentTheme === defaultTheme ? bgLight : bgDark;
 
 	// Runs only once
 	setTimeout(function () {
@@ -28,10 +33,14 @@ const main = function () {
 			html.classList.remove('dark');
 			html.classList.add('light');
 			setTheme('light');
+			// Change meta 'theme-color'
+			themeColor.content = bgLight;
 		} else {
 			html.classList.remove('light');
 			html.classList.add('dark');
 			setTheme('dark');
+			// Change meta 'theme-color'
+			themeColor.content = bgDark;
 		}
 	});
 };
